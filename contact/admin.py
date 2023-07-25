d5a07edce346d4da7c81b3e362348c76eac71881
Pedro Sartori <pedrosartdr@gmail.com>
2023-07-25 01:58:24 -0300

    model contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = ('-id',)
+    list_per_page = 10
+    list_max_show_all = 200
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
+from typing import Any
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+
+    def __str__(self) -> str:
+        return self.first_name.capitalize() + ' ' + self.last_name.capitalize()
+
+
