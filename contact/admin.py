007c2cad742a6b2025111ed04ad33dea9ae95dc9
Pedro Sartori <pedrosartdr@gmail.com>
2023-07-26 19:40:56 -0300

    view in package

--- contact/admin.py
+++ contact/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from contact.models import Contact
+from contact.models import Category
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    ordering = ('name',)
+    list_per_page = 10
+    list_max_show_all = 200
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'category')
     ordering = ('-id',)
     list_per_page = 10
     list_max_show_all = 200
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,18 @@
 from typing import Any
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name.capitalize()
 
 
 class Contact(models.Model):
@@ -12,6 +24,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.first_name.capitalize() + ' ' + self.last_name.capitalize()
