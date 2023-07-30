4f5a821fa4cd47de92bc27e6a059d7e95d92cc47
Pedro Sartori <pedrosartdr@gmail.com>
2023-07-29 21:10:17 -0300

    validating fields

--- contact/forms.py
+++ contact/forms.py
@@ -4,12 +4,43 @@ from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            {
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio da classe'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para eu usuário'
+    )
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = ('first_name', 'last_name', 'phone', 'description')
+        # widgets = {
+        #     'first_name': forms.TextInput({
+        #         'class': 'class-a class-b'
+        #     })
+        # }
     
-    # def clean(self) -> Dict[str, Any]:
-    #     cleaned_data = self.cleaned_data
-    #     self.add_error('first_name', ValidationError('Mensagem de erro', 'invalid'))
-    #     self.add_error('first_name', ValidationError('Mensagem de erro 2', 'invalid'))
-    #     return super().clean()
+    def clean(self) -> dict[str, any]:
+        first_name: str = self.cleaned_data.get('first_name')
+        last_name: str = self.cleaned_data.get('last_name')
+
+        if first_name.upper() == last_name.upper():
+            self.add_error('first_name', ValidationError("First name can't be the same as the last name"))
+            self.add_error('last_name', ValidationError("Last name can't be the same as the first name"))
+
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name: str = self.cleaned_data.get('first_name')
+        
+        if first_name.upper() in ['ATC', 'BOI', 'UTA']:
+            self.add_error(
+                'first_name',
+                ValidationError("Don't use bad words.")
+            )
+
+        return first_name
